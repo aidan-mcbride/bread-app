3e900a62f2ca0de772e0653de044b4e7a1f5109f
aidan-mcbride <aidanmcbride@outlook.com>
2019-11-02 14:28:22 -0600

    Add endpoint for creating recipe
    Endpoint currently does nothing, and does not accept the correct request 
    body, but it exists, and there is a test to confirm that it does indeed 
    exist.

--- api/main.py
+++ api/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from starlette.status import HTTP_201_CREATED
 
 app = FastAPI()
 
@@ -6,3 +8,16 @@ app = FastAPI()
 @app.get("/")
 async def read_main():
     return {"msg": "Hello World"}
+
+
+# TODO: move to schemas dir or something
+# TODO: add remaining fields
+class Recipe(BaseModel):
+    name: str
+    rating: int
+
+
+# TODO: move to router file
+@app.post("/recipes/", status_code=HTTP_201_CREATED)
+async def create_recipe(recipe: Recipe):
+    return recipe
--- tests/test_main.py
+++ tests/test_main.py
@@ -9,3 +9,29 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
+
+
+mock_recipe = {
+    "creator": 1,
+    "dateCreated": "ISO-8601 date",
+    "ingredients": [
+        {"name": "salt", "quantity": 1.5, "unit": "tsp"},
+        {"name": "all-purpose flour", "quantity": 1, "unit": "cups"},
+    ],
+    "procedures": [
+        {"name": "proof", "time": "90", "temp": 80, "details": "oven bottom rack"},
+        {"name": "bake", "time": 10, "temp": 500},
+    ],
+    "shape": "snack round",
+    "yield": 8,
+    "results": {"rating": 5},
+}
+
+
+def test_create_recipe():
+    response = client.post("/recipes/", json={"name": "bread", "rating": 5})
+
+    # test status code
+    actual = response.status_code
+    expected = 201
+    assert expected == actual
