75a41fd3ea596270c540e17aaff92ded520a1803
aidan-mcbride <aidanmcbride@outlook.com>
2019-11-03 10:05:43 -0700

    Add utility functions to create random test data

--- tests/test_recipes.py
+++ tests/test_recipes.py
@@ -1,10 +1,54 @@
+import random
+import string
 from datetime import date
 
 from starlette.testclient import TestClient
 
 from api.main import app
+from api.schemas.recipe import Ingredient, Procedure, Recipe, Unit
 
 client = TestClient(app)
+
+
+def random_lower_string(length: int = 32) -> str:
+    return str().join(random.choices(string.ascii_lowercase, k=length))
+
+
+def random_ingredient() -> Ingredient:
+    ingredient = Ingredient(
+        name=random_lower_string(length=12),
+        quantity=random.uniform(1, 10),
+        unit=random.choice(list(Unit)),
+    )
+    return ingredient
+
+
+def random_procedure() -> Procedure:
+    procedure = Procedure(
+        name=random_lower_string(length=12),
+        time=random.randint(5, 120),
+        temperature=random.randint(10, 600),
+        details=random_lower_string(length=random.randint(24, 300)),
+    )
+    return procedure
+
+
+def random_recipe() -> Recipe:
+    recipe = Recipe(
+        shape=random_lower_string(),
+        servings=random.randint(1, 24),
+        rating=random.randint(1, 5),
+        notes=random_lower_string(length=300),
+    )
+    for _ in range(random.randint(1, 12)):
+        recipe.ingredients.append(random_ingredient())
+    for _ in range(random.randint(1, 12)):
+        recipe.procedures.append(random_procedure())
+    return recipe
+
+
+class TestReadRecipes:
+    pass
 
 
 class TestCreateRecipe:
