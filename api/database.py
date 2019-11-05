0d03f0a1650245855add0e63b2ee11916ee2b454
aidan-mcbride <aidanmcbride@outlook.com>
2019-11-05 14:50:35 -0700

    Add database connection and test database
    Add pytest fixture to drop database collections before and after running
    tests
    
    I actually got my database operation functions working at the same time
    with TDD, but - to avoid having one giant commit - I am committing this
    in sections. I disabled pytest for this because, until all my changes
    are committed, the tests will fail.

--- api/database.py
+++ api/database.py
@@ -3,8 +3,24 @@ MOCK DATABASE FOR NOW
 TODO: Replace with ArangoDB
 """
 
-db: dict = dict(recipes=list(), users=list())
+from pyArango.collection import Collection
+from pyArango.connection import Connection
+from pyArango.database import Database
 
 
-def get_db():
+def get_db(db_name: str = "breadapp") -> Database:
+    conn = Connection()
+    if not conn.hasDatabase(db_name):
+        conn.createDatabase(name=db_name)
+    db = conn[db_name]
     return db
+
+
+def get_test_db(db_name: str = "breadapp_testing") -> Database:
+    return get_db(db_name=db_name)
+
+
+def get_collection(db: Database, collection: str) -> Collection:
+    if not db.hasCollection(name=collection):
+        db.createCollection(name=collection)
+    return db[collection]
