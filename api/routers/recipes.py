fdfde57b4eae312b1b1be86318c6bc5a1d441157
aidan-mcbride <aidanmcbride@outlook.com>
2019-11-08 13:16:36 -0700

    Fix type declaration for database dependency in routes

--- api/routers/recipes.py
+++ api/routers/recipes.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from fastapi import APIRouter, Depends, HTTPException
+from pyArango.database import Database
 from starlette.status import HTTP_201_CREATED, HTTP_404_NOT_FOUND
 
 from api import db_ops
@@ -11,18 +12,20 @@ router = APIRouter()
 
 
 @router.post("/", status_code=HTTP_201_CREATED, response_model=Recipe)
-async def create_recipe(recipe_in: RecipeCreate, db: dict = Depends(get_db)) -> Recipe:
+async def create_recipe(
+    recipe_in: RecipeCreate, db: Database = Depends(get_db)
+) -> Recipe:
     return db_ops.create_recipe(db=db, recipe_in=recipe_in)
 
 
 # @router.get("/", response_model=Recipe)
 @router.get("/")
-def read_recipes(db: dict = Depends(get_db)) -> List[Recipe]:
+def read_recipes(db: Database = Depends(get_db)) -> List[Recipe]:
     return db_ops.read_recipes(db=db)
 
 
 @router.get("/{id}", response_model=Recipe)
-def read_recipe(id: int, db: dict = Depends(get_db)) -> Recipe:
+def read_recipe(id: int, db: Database = Depends(get_db)) -> Recipe:
     recipe: Recipe = db_ops.read_recipe(db=db, id=id)
     if not recipe:
         raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="Recipe not found")
