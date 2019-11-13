08880334237ce0e67bfa3bcc70186d289fc3505d
aidan-mcbride <aidanmcbride@outlook.com>
2019-11-13 09:30:12 -0700

    Refactor user models: first pass

--- api/schemas/user.py
+++ api/schemas/user.py
@@ -3,27 +3,49 @@ from typing import Optional
 from pydantic import BaseModel, EmailStr  # , Schema
 
 
+# base class for all users
 class UserBase(BaseModel):
-    email: Optional[EmailStr] = None
-    is_active: Optional[bool] = True
+    pass
 
 
+# base class for users that have been saved to database
+class UserBaseInDB(UserBase):
+    pass
+
+
+# fields available to client when creating a new user
 class UserCreate(UserBase):
     email: EmailStr
     password: str
+    is_active: Optional[bool] = True
 
 
-# data added to created recipe before being added to db
-class UserCreateToDB(UserBase):
+# hashed_password added on server before saving to db
+class UserCreateToDB(UserCreate):
+    email: EmailStr
+    password: str
+    is_active: Optional[bool] = True
+    # ---
     hashed_password: str
 
 
-class UserUpdate(UserBase):
+# fields available to client when updating existing user
+class UserUpdate(UserBaseInDB):
     email: Optional[EmailStr] = None
     password: Optional[str] = None
+    is_active: Optional[bool] = True
 
 
-# data returned to client as response body
-class User(UserBase):
+# all data in database: used for internal use in api (auth)
+class UserInDB(UserBaseInDB):
     id: int
     is_active: bool
+    email: EmailStr
+    hashed_password: str
+
+
+# data returned to client as response body for all requests
+class User(UserBaseInDB):
+    id: int
+    is_active: bool
+    email: EmailStr
