d15562ec592127bae4d91530f303edb6fcd58bab
aidan-mcbride <aidanmcbride@outlook.com>
2019-11-13 10:00:42 -0700

    Refactor user models: second pass

--- api/schemas/user.py
+++ api/schemas/user.py
@@ -5,32 +5,31 @@ from pydantic import BaseModel, EmailStr  # , Schema
 
 # base class for all users
 class UserBase(BaseModel):
-    pass
+    email: Optional[EmailStr] = None
+    password: Optional[str] = None
+    is_active: Optional[bool] = True
 
 
 # base class for users that have been saved to database
 class UserBaseInDB(UserBase):
-    pass
+    id: int
+    email: EmailStr
+    is_active: bool
 
 
 # fields available to client when creating a new user
 class UserCreate(UserBase):
     email: EmailStr
     password: str
-    is_active: Optional[bool] = True
 
 
 # hashed_password added on server before saving to db
 class UserCreateToDB(UserCreate):
-    email: EmailStr
-    password: str
-    is_active: Optional[bool] = True
-    # ---
     hashed_password: str
 
 
 # fields available to client when updating existing user
-class UserUpdate(UserBaseInDB):
+class UserUpdate(UserBase):
     email: Optional[EmailStr] = None
     password: Optional[str] = None
     is_active: Optional[bool] = True
@@ -38,14 +37,9 @@ class UserUpdate(UserBaseInDB):
 
 # all data in database: used for internal use in api (auth)
 class UserInDB(UserBaseInDB):
-    id: int
-    is_active: bool
-    email: EmailStr
     hashed_password: str
 
 
 # data returned to client as response body for all requests
 class User(UserBaseInDB):
-    id: int
-    is_active: bool
-    email: EmailStr
+    pass
