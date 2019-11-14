db3551a8a47dea96146e2ee25e9c5aa5363454ea
aidan-mcbride <aidanmcbride@outlook.com>
2019-11-14 14:48:32 -0700

    Add read users endpoint

--- api/routers/users.py
+++ api/routers/users.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import APIRouter, Depends, HTTPException
 from pyArango.database import Database
 from starlette.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
@@ -11,7 +13,7 @@ router = APIRouter()
 
 
 @router.post("/", status_code=HTTP_201_CREATED, response_model=User)
-def create_recipe(user_in: UserCreate, db: Database = Depends(get_db)) -> User:
+def create_recipe(user_in: UserCreate, db: Database = Depends(get_db)) -> UserInDB:
     user = db_ops.users.read_by_email(db=db, email=user_in.email)
     print(user)
     if user:
@@ -19,9 +21,14 @@ def create_recipe(user_in: UserCreate, db: Database = Depends(get_db)) -> User:
             status_code=HTTP_400_BAD_REQUEST,
             detail="A user with this email address already exists.",
         )
-    user = db_ops.users.create(db=db, user_in=user_in)
-    response_data = User(**user.dict())
-    return response_data
+    return db_ops.users.create(db=db, user_in=user_in)
+
+
+@router.get("/", response_model=List[User])
+def read_users(
+    db: Database = Depends(get_db), current_user: UserInDB = Depends(get_current_user)
+) -> List[UserInDB]:
+    return db_ops.users.read_all(db=db)
 
 
 @router.get("/me", response_model=User)
--- tests/routers/test_routers_users.py
+++ tests/routers/test_routers_users.py
@@ -1,8 +1,13 @@
-from starlette.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from starlette.status import (
+    HTTP_200_OK,
+    HTTP_201_CREATED,
+    HTTP_400_BAD_REQUEST,
+    HTTP_401_UNAUTHORIZED,
+)
 from starlette.testclient import TestClient
 
 from api.main import app
-from tests.utils import TESTING_USER_EMAIL
+from tests.utils import TESTING_USER_EMAIL, create_random_user
 
 client = TestClient(app)
 
@@ -36,6 +41,32 @@ class TestCreateUser:
         assert len(actual) == 1
 
 
+class TestReadUsers:
+    def test_read(self, test_user_token_headers):
+        for _ in range(3):
+            create_random_user()
+
+        response = client.get("/users/", headers=test_user_token_headers)
+
+        actual = response.status_code
+        expected = HTTP_200_OK
+        assert expected == actual
+
+        actual = len(response.json())
+        expected = 3 + 1  # 3 randoms + test user with token
+        assert expected == actual
+
+        for user in response.json():
+            assert "id" in user
+            assert "password" not in user
+            assert "hashed_password" not in user
+
+    def test_read_no_access(self):
+        actual = client.get("/users/").status_code
+        expected = HTTP_401_UNAUTHORIZED
+        assert expected == actual
+
+
 class TestReadCurrentUser:
     # test_user_token_headers is a pytest fixture in conftest.py
     def test_read(self, test_user_token_headers):
@@ -43,4 +74,9 @@ class TestReadCurrentUser:
 
         actual = response.json()["email"]
         expected = TESTING_USER_EMAIL
+        assert expected == actual
+
+    def test_read_not_authorized(self):
+        actual = client.get("/users/me").status_code
+        expected = HTTP_401_UNAUTHORIZED
         assert expected == actual
