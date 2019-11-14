a95f4a504686d3b4208a85133ec0327818f8461a
aidan-mcbride <aidanmcbride@outlook.com>
2019-11-14 08:58:49 -0700

    Add get_current_user security function

--- api/security.py
+++ api/security.py
@@ -2,7 +2,20 @@ import os
 from datetime import datetime, timedelta
 
 import jwt
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jwt import PyJWTError
 from passlib.context import CryptContext
+from pyArango.database import Database
+from starlette.status import HTTP_403_FORBIDDEN, HTTP_404_NOT_FOUND
+
+from api import db_ops
+from api.database import get_db
+
+# from api.schemas.token import TokenPayload
+
+# ---------------------------------------------
+
 
 pw_ctx = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -15,8 +28,11 @@ def verify_password_hash(plain_password: str, hashed_password: str):
     return pw_ctx.verify(plain_password, hashed_password)
 
 
+# ---------------------------------------------
+
+# TODO: move to some config file
 SECRET_KEY = str(os.getenv("JWT_SECRET_KEY"))
-ALGORITH = str(os.getenv("JWT_ALGORITHM"))
+ALGORITHM = str(os.getenv("JWT_ALGORITHM"))
 
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
@@ -26,5 +42,33 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITH)
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+# ---------------------------------------------
+
+
+# tokenUrl: the url where the client submits their credentials to get a token
+# https://fastapi.tiangolo.com/tutorial/security/first-steps/#fastapis-oauth2passwordbearer
+get_token = OAuth2PasswordBearer(tokenUrl="/login")
+
+
+def get_current_user(db: Database = Depends(get_db), token: str = Depends(get_token)):
+    """
+    decodes the given JWT token to obtain a user id,
+    then returns that user in the database
+    """
+    # decode JWT token
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id = payload["user_id"]
+    except PyJWTError:
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN, detail="Could not validate credentials"
+        )
+    # read and return user in db
+    user = db_ops.users.read(db=db, id=user_id)
+    if not user:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="User not found")
+    return user
